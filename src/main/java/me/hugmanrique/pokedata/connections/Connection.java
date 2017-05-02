d000caf695dc2ea162c388f646fa1c7d8e5685ee
hugmanrique <hugmanrique@gmail.com>
2017-05-01 21:05:30 -0400

    End Connection data classes

---
+++ src/main/java/me/hugmanrique/pokedata/connections/Connection.java
@@ -0,0 +1,27 @@
+package me.hugmanrique.pokedata.connections;
+
+import lombok.Getter;
+import me.hugmanrique.pokedata.Data;
+import me.hugmanrique.pokedata.utils.ROM;
+
+/**
+ * Missing bulbapedia article
+ * @author Hugmanrique
+ * @since 01/05/2017
+ */
+@Getter
+public class Connection extends Data {
+    private long type;
+    private long offset;
+    private byte bank;
+    private byte map;
+    private int filler;
+
+    public Connection(ROM rom) {
+        type = rom.getPointer(true);
+        offset = rom.getSignedLong(true);
+        bank = rom.readByte();
+        map = rom.readByte();
+        filler = rom.readWord();
+    }
+}
---
+++ src/main/java/me/hugmanrique/pokedata/connections/ConnectionData.java
@@ -0,0 +1,38 @@
+package me.hugmanrique.pokedata.connections;
+
+import lombok.Getter;
+import me.hugmanrique.pokedata.Data;
+import me.hugmanrique.pokedata.utils.BitConverter;
+import me.hugmanrique.pokedata.utils.ROM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bulbapedia article missing
+ * @author Hugmanrique
+ * @since 01/05/2017
+ */
+@Getter
+public class ConnectionData extends Data {
+    private long numConnsPtr;
+    private long dataPtr;
+
+    private List<Connection> connections;
+
+    public ConnectionData(ROM rom) {
+        numConnsPtr = rom.getPointer(true);
+        dataPtr = rom.getPointer(true);
+
+        connections = new ArrayList<>();
+        loadConnections(rom);
+    }
+
+    private void loadConnections(ROM rom) {
+        rom.seek(BitConverter.shortenPointer(dataPtr));
+
+        for (int i = 0; i < numConnsPtr; i++) {
+            connections.add(new Connection(rom));
+        }
+    }
+}
