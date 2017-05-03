321100a187dd734102ecfc0272a381864df66dac
hugmanrique <hugmanrique@gmail.com>
2017-05-02 20:55:16 -0400

    Implement Triggers

--- src/main/java/me/hugmanrique/pokedata/sprites/triggers/Trigger.java
+++ src/main/java/me/hugmanrique/pokedata/sprites/triggers/Trigger.java
@@ -1,38 +1,38 @@
-package me.hugmanrique.pokedata.sprites.signs;
+package me.hugmanrique.pokedata.sprites.triggers;
 
 import lombok.Getter;
 import me.hugmanrique.pokedata.sprites.Sprite;
 import me.hugmanrique.pokedata.utils.ROM;
 
 /**
+ * Although triggers aren't sprites, they have the same base structure
  * @author Hugmanrique
  * @since 02/05/2017
  */
 @Getter
-public class SpriteSign extends Sprite {
-    // TODO Check if some of these bytes are words values
+public class Trigger extends Sprite {
     private byte b2;
     private byte b4;
-    private byte b5;
-    private byte b6;
-    private byte b7;
-    private byte b8;
+    private int h3;
+    private int flagCheck;
+    private int flagValue;
+    private int h6;
     private long scriptPtr;
 
-    public SpriteSign(ROM rom) {
+    public Trigger(ROM rom) {
         x = rom.readByte();
         b2 = rom.readByte();
         y = rom.readByte();
         b4 = rom.readByte();
-        b5 = rom.readByte();
-        b6 = rom.readByte();
-        b7 = rom.readByte();
-        b8 = rom.readByte();
+        h3 = rom.readWord();
+        flagCheck = rom.readWord();
+        flagValue = rom.readWord();
+        h6 = rom.readWord();
         scriptPtr = rom.getPointer();
     }
 
     @Override
     public int getSize() {
-        return 12;
+        return 16;
     }
 }
---
+++ src/main/java/me/hugmanrique/pokedata/sprites/triggers/TriggerManager.java
@@ -0,0 +1,29 @@
+package me.hugmanrique.pokedata.sprites.triggers;
+
+import me.hugmanrique.pokedata.sprites.SpriteManager;
+import me.hugmanrique.pokedata.utils.ROM;
+
+import java.util.List;
+
+/**
+ * Explanation on the {@link SpriteManager} inheritance
+ * is available in the {@link Trigger}'s javadocs
+ * @author Hugmanrique
+ * @since 02/05/2017
+ */
+public class TriggerManager extends SpriteManager<Trigger> {
+    public TriggerManager(ROM rom, int count) {
+        for (int i = 0; i < count; i++) {
+            sprites.add(new Trigger(rom));
+        }
+    }
+
+    public TriggerManager(ROM rom, int offset, int count) {
+        super(rom, offset, count);
+    }
+
+    // Getter mapping, triggers aren't sprites
+    public List<Trigger> getTriggers() {
+        return sprites;
+    }
+}
