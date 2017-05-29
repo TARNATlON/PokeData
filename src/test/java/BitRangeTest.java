9dd2e441238f11ceeeefe989f28a463205ed4945
hugmanrique <hugmanrique@gmail.com>
2017-05-28 23:42:31 -0400

    Remove test

--- src/test/java/BitRangeTest.java
+++ src/test/java/BitRangeTest.java
@@ -13,10 +13,4 @@ public class BitRangeTest {
         assertEquals(0b10, BitConverter.getBitRange(0b1000000000000000, 0, 2));
         assertEquals(0b10, BitConverter.getBitRange(0b0010000000000000, 2, 4));
     }
-
-    @Test
-    public void testArbitraryLength() {
-        // TODO Fix
-        //assertEquals(0b1, BitConverter.getBitRange(0b00100, 3, 4));
-    }
 }
