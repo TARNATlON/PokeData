6f50de25f4b9e19452d97da9c6644e5ef53c4657
Hugo Manrique <hugmanrique@gmail.com>
2017-08-06 19:42:49 +0200

    Add Map loading test

--- src/test/java/MapLoadingTest.java
+++ src/test/java/MapLoadingTest.java
@@ -1,5 +1,8 @@
+import me.hugmanrique.pokedata.loaders.ROMData;
 import me.hugmanrique.pokedata.maps.Map;
-import me.hugmanrique.pokedata.roms.Game;
+import me.hugmanrique.pokedata.maps.MapData;
+import me.hugmanrique.pokedata.maps.MapHeader;
+import me.hugmanrique.pokedata.maps.MapTileData;
 import me.hugmanrique.pokedata.roms.ReadableROM;
 import org.junit.Test;
 
@@ -13,9 +16,39 @@ public class MapLoadingTest extends ROMTest {
 
         checkFireRed(rom);
 
+        ROMData data = loadData(rom);
 
-        // Pallet town is
+        // Pallet town is in bank 3, map 0
         Map map = Map.load(rom, data, 3, 0);
 
+        // Check header
+        MapHeader header = map.getHeader();
+
+        checkPointer("Map data", header.getMapPtr(), 0x2DD4C0);
+        checkPointer("Event data", header.getSpritesPtr(), 0x3B4E50);
+        checkPointer("Map scripts", header.getScriptPtr(), 0x16545A);
+        checkPointer("Connections", header.getConnectPtr(), 0x35276C);
+
+        // Check data
+        MapData mapData = map.getData();
+
+        checkPointer("Border", mapData.getBorderTilePtr(), 0x2DD0F8);
+        checkPointer("Tile structure", mapData.getMapTilesPtr(), 0x2DD100);
+        checkPointer("Global tileset", mapData.getGlobalTilesetPtr(), 0x2D4A94);
+        checkPointer("Local tileset", mapData.getLocalTilesetPtr(), 0x2D4AAC);
+
+        // TODO Check scripts, connections...
+    }
+
+    private void checkPointer(String name, long pointer, long expected) {
+        if (pointer != expected) {
+            throwFail(name, pointer, expected);
+        }
+    }
+
+    private void throwFail(String name, Object obj, Object comparedTo) {
+        String message = String.format("%s don't/doesn't match (got %s, expected: %s)", name, obj, comparedTo);
+
+        throw new Error(message);
     }
 }
